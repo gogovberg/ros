875af675481fd60bae57801fcc8253da2932a5e5
Zorko <zorkojaka@gmail.com>
2017-11-13 15:58:10 +0100

    task one done

--- src/evaluation_tasks/src/task_one/cliff_listener.cpp
+++ src/evaluation_tasks/src/task_one/cliff_listener.cpp
@@ -12,24 +12,76 @@ using namespace std;
 
 ros::Publisher pub;
 geometry_msgs::Twist base_cmd;
+bool isChange = false;
+double cliffThreshold = 1800; 
+double drive = 0;
+double turn = 1;
+
 void callbackCliff(const create_node::TurtlebotSensorState& cliff_msg)
 { 
   
   try
     {
-  
-      ROS_INFO("Cliff front left signal: %d",cliff_msg.cliff_front_left_signal);
-      ROS_INFO("Cliff front right signal: %d",cliff_msg.cliff_front_right_signal);
-
-      base_cmd.linear.y = 0;
+  	  base_cmd.linear.y = 0;
       base_cmd.linear.z = 0;
       base_cmd.angular.x = 0;
       base_cmd.angular.y = 0;
-      base_cmd.angular.z = 5;
-      base_cmd.linear.x = 10;
-      ROS_INFO("BASE CMD %f",base_cmd.linear.x);
-      pub.publish(base_cmd);
-
+      
+      //this is what we use for moving
+      base_cmd.angular.z = 0;
+      base_cmd.linear.x = 0;
+      
+      ROS_INFO("Cliff left signal: %d",cliff_msg.cliff_left_signal);
+      ROS_INFO("Cliff front left signal: %d",cliff_msg.cliff_front_left_signal);
+ 	  ROS_INFO("Cliff front right signal: %d",cliff_msg.cliff_front_right_signal);
+      ROS_INFO("Cliff right signal: %d",cliff_msg.cliff_right_signal);
+      
+     
+      if(cliff_msg.cliff_left_signal < cliffThreshold)
+      {
+      	 turn = 1;
+      	 drive = 0;
+      	 isChange = true;
+      }
+      else if(cliff_msg.cliff_front_left_signal< cliffThreshold)
+      {
+      	 turn = 0.2;
+      	 drive = 0.2;
+      	 isChange = true;
+      }
+      else if(cliff_msg.cliff_front_right_signal< cliffThreshold)
+      {
+     	 
+      	 turn = -0.2;
+      	 drive = 0.2;
+      	 isChange = true;
+     	       	
+      }
+      else if(cliff_msg.cliff_right_signal < cliffThreshold)
+      {
+     	 
+      	 turn = -1;
+      	 drive = 0;
+     	 isChange = true;
+      }
+      else
+      {
+      	if(!isChange)
+		{
+			drive= 0.2;
+			turn = 0.5;
+		}
+		else
+		{
+			drive = 0.2;
+		}
+	  }
+      
+      base_cmd.angular.z = turn;
+      base_cmd.linear.x = drive;
+      
+      ROS_INFO("BASE CMD drive %f",base_cmd.linear.x);
+      ROS_INFO("BASE CMD turn %f",base_cmd.angular.z);
   }
   catch ( ros::Exception &e )
   {
@@ -47,10 +99,10 @@ int main (int argc, char** argv) {
   ros::Subscriber sub = nh.subscribe ("/mobile_base/sensors/core", 1, callbackCliff);
   ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
   // Spin
- while(ros::ok){
+  while(ros::ok){
     pub.publish(base_cmd);
     ros::spinOnce();
-    }
+  }
  //ros::spin();
  return 0;
 }
